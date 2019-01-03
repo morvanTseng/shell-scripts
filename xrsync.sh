ec2f4612919b10fbeb714a938135dc1e81213f92
morvantseng <permanentzenghao@icloud.com>
2019-01-03 22:07:43 +0800

    2019-1-3

---
+++ xrsync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this script is for the purpose of sync files on different machines
+
+# user
+
+user=$(whoami)
+
+# synced  file
+
+file_path=$1
+
+#hosts
+
+hosts=$2
+
+# destination_path
+
+destination_path=$3
+
+
+cat $hosts | while read host
+do
+    echo "----------$host---------"
+    rsync -rvl ${file_path} ${user}@${host}:$destination
+    echo "----------done---------"
+done
